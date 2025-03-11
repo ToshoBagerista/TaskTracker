2922dc0f54a18c825d220bc6aa28791cf63705b4
Vancho <ivancvetkov116@gmail.com>
2025-03-11 18:59:54 +0200

    Added error handling

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import os, json
-import time
 
 from Task import Task
 
@@ -17,18 +16,17 @@ def Load():
 	global data, ID, tasks
 	try:
 		loadtasks = json.load(data)
-		# print(loadtasks)
 		for task in loadtasks.values():
 			tasks[ID] = Task(ID, task['description'], status=task['status'], createdAt=task['createdAt'], updatedAt=task['updatedAt'])
 			ID += 1
-		# time.sleep(0.1)
 	except ValueError:
 		pass
 	data.close()
 	os.remove("data.json")
 
 def Delete(id):
-	del tasks[id]
+	try: del tasks[id]
+	except KeyError: print("Task not found!")
 
 def Update(id, description):
 	tasks[id].update(description)
@@ -46,51 +44,66 @@ def mark(id, status):
 
 def menu():
 	global isActive
-	inp = input("Enter your command: ")
-	if inp.__contains__("\""): com = list(inp[:inp.index("\"")].split(" "))
-	else: com = list(inp.split(" "))
-	if com[0] == "task-cli":
-		match(com[1]):
-			case "add":
-				Add(inp[inp.index("\"")+1:len(inp)-1])
-				# print(tasks[1].description)
-			case "delete":
-				Delete(int(com[2]))
-				return
-			case "update":
-				Update(int(com[2]), inp[inp.index("\"")+1:len(inp)-1])
-				return
-			case "list":
-				if len(com) == 2: listtasks("all")
-				else:
-					match(com[2]):
-						case "done":
-							listtasks("done")
-							return
-						case "todo":
-							listtasks("todo")
-							return
-						case "in-progress":
-							listtasks("in-progress")
-							return
-				return
-			case "mark-in-progress":
-				mark(int(com[2]), "in-progress")
-				return
-			case "mark-done":
-				mark(int(com[2]), "done")
-			case "exit":
-				isActive = False
+	try:
+		inp = input("Enter your command: ")
+		if inp.__contains__("\""): com = list(inp[:inp.index("\"")].split(" "))
+		else: com = list(inp.split(" "))
+		if com[0] == "task-cli":
+			match(com[1]):
+				case "add":
+					Add(inp[inp.index("\"")+1:len(inp)-1])
+					return
+				case "delete":
+					Delete(int(com[2]))
+					return
+				case "update":
+					Update(int(com[2]), inp[inp.index("\"")+1:len(inp)-1])
+					return
+				case "list":
+					if len(com) == 2: listtasks("all")
+					else:
+						match(com[2]):
+							case "done":
+								listtasks("done")
+								return
+							case "todo":
+								listtasks("todo")
+								return
+							case "in-progress":
+								listtasks("in-progress")
+								return
+					return
+				case "mark-in-progress":
+					mark(int(com[2]), "in-progress")
+					return
+				case "mark-done":
+					mark(int(com[2]), "done")
+					return
+				case "exit":
+					isActive = False
+					return
+	except ValueError: print("Invalid input! Try again!")
+	except IndexError: print("Something went wrong! Try again!")
+	except KeyError: print("Something went wrong! Try again!")
+	finally: return
 
 def main():
 	global data
-	if os.path.isfile("data.json"):
-		data = open("data.json", "r")
-		Load()
-	data = open("data.json", "w")
+	try:
+		if os.path.isfile("data.json"):
+			data = open("data.json", "r")
+			Load()
+		data = open("data.json", "w")
+	except BaseException:
+		if os.path.isfile("data.json"):
+			data.close()
+			os.remove("data.json")
+			data = open("data.json", "w")
+		else: data.open("data.json", "w")
 	while isActive:
 		menu()
-	json.dump({int(k): v.__str__() for (k, v) in tasks.items()}, data, indent=2)
+	try: json.dump({int(k): v.__str__() for (k, v) in tasks.items()}, data, indent=2)
+	except BaseException: print("You did something wrong! You are an idiot!")
 
 if __name__ == "__main__":
 	main()
--- Task.py
+++ Task.py
@@ -16,12 +16,7 @@ class Task:
 		self.description = description
 
 	def updateStatus(self, status):
+		self.updatedAt = datetime.now()
 		self.status = status
-
-	def to_json(self):
-		return self
-
-	def __dict__(self):
-		return {"id": self.id, 'description': self.description, 'status': self.status, 'createdAt': self.createdAt, 'updatedAt': self.updatedAt}
 
 	def __str__(self): return {"id": self.id, 'description': self.description, 'status': self.status, 'createdAt': str(f"{self.createdAt :%d/%m/%y %H:%M:%S}"), 'updatedAt': str(f"{self.updatedAt :%d/%m/%y %H:%M:%S}")}
